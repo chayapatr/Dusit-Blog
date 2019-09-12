7dff2bd4f64a51b45ab9700277e45adac4624971
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-09-13 01:01:30 +0700

    :wrench: Fix the Card capital letter since git never detect the capital letter changes

---
+++ components/card/index.jsx
@@ -0,0 +1,37 @@
+/* import { h, Fragment } from 'preact' */
+import React, { Fragment } from 'react'
+
+import Link from 'next/link'
+
+import './card.styl'
+
+const Card = props => {
+    let {
+        title,
+        children = '',
+        src,
+        alt = 'Image cover',
+        tag,
+    } = props
+
+    return (
+        <Link href={`/blog/${title}`}>
+            <a className="link">
+                <article className="card">
+                    <img className="cover" src={src} alt={alt} />
+                    <h5 className="title">{title}</h5>
+                    <p className="detail">{children}</p>
+                    <div className="tag-container">
+                        {tag.map((tagName, index) => (
+                            <Link key={index} href={`/category/${tagName}`}>
+                                <a className="tag">{tagName}</a>
+                            </Link>
+                        ))}
+                    </div>
+                </article>
+            </a>
+        </Link>
+    )
+}
+
+export default Card
